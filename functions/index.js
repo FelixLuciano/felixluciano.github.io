b730dfa13103585193e2fe4dc69e05b157cc7c77
FelixLuciano <felix.lucianodias@gmail.com>
2021-01-17 00:47:49 -0300

    Fixed Send Message Firebase Function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-const functions = require('firebase-functions')
-const cors = require('cors')
-const nodemailer = require('nodemailer')
+const functions = require("firebase-functions")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
 
-const corsHandler = cors({ origin: 'https://lucianofelix.com.br' })
+const corsHandler = cors({ origin: "https://lucianofelix.com.br" })
 
 /* Firebase Enviroment variables
  / https://firebase.google.com/docs/functions/config-env
@@ -12,10 +12,11 @@ const corsHandler = cors({ origin: 'https://lucianofelix.com.br' })
  /   to: <resiving email addresss>
  /}
 */
+console.log(functions.config())
 const { user, pass, to } = functions.config().nodemailer
 
 const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: { user, pass }
 })
 
@@ -26,16 +27,20 @@ const request = (req, res) => {
   return corsHandler(req, res, () => {
     const { contact, message:plainMessage } = req.body
 
-    const message = plainMessage.replace(/\n/gm, '<br>')
+    const message = plainMessage.replace(/\n/gm, "<br>")
 
     const mail = {
       from: `LucianoFelix.com.br <${user}>`,
       to: to,
+      subject: "Nova Mensagen",
       html: `<b>Contact:</b><div style="padding-left:1ex">${contact}</div><br><b>Message:</b><blockquote class="gmail_quote" style="margin:.5ex 0 0 1ex;border-left:1px solid #000;padding-left: 1ex;">${message}</blockquote>`
     }
-
+    
     mailTransport.sendMail(mail, err => {
-      if (err) res.status(400).json(false)
+      if (err) {
+        res.status(400).json(false)
+        console.log(err)
+      }
       else res.status(200).json(true)
     })
   })
