ae7594a1ee6ee848d7e91504e9d0b6a804e2fa61
FelixLuciano <felix.lucianodias@gmail.com>
2021-01-23 03:33:52 -0300

    Assets compression

--- src/components/section-about-me.vue
+++ src/components/section-about-me.vue
@@ -4,7 +4,7 @@ content-section#About-me.max-w-screen-md
   template(v-slot:title) WHO I AM
 
   article
-    img#profile-photo(src="/images/profile-photo.jpg" alt="Profile photo")
+    img#profile-photo(src="/images/profile-photo.webp" alt="Profile photo")
 
   article#letters.pt-8.md_text-lg.no-wrap
     letters
