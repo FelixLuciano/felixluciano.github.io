c1e383a3bcbc3be6e3e4a0a631a03ec67236219b
FelixLuciano <felix.lucianodias@gmail.com>
2021-01-28 12:30:07 -0300

    Update dependencies

--- src/components/post-card.vue
+++ src/components/post-card.vue
@@ -21,7 +21,7 @@ figure
 <script>
 
 import { getCurrentInstance, onMounted } from "vue"
-import MediumZoom from "medium-zoom"
+import MediumZoom from "https://cdnjs.cloudflare.com/ajax/libs/medium-zoom/1.0.6/medium-zoom.esm.js"
 
 export default {
   props: {
